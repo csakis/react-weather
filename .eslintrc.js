2c0e9496e1ae82767878964218fedce05ce2cfa0
Csaba Kiss <csakis@gmail.com>
2021-10-17 12:54:09 -0600

    project basic setup

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+
+  },
+};
